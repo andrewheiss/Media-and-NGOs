3232df7004d2d12af0456935117f74db23624195
Andrew Heiss <andrewheiss@gmail.com>
2013-07-01 15:50:26 -0400

    Add better documentation

--- clean_files.py
+++ clean_files.py
@@ -1,12 +1,30 @@
 #!/usr/bin/env python3
+
+# Title:          clean_files.py
+# Description:    Filter a directory of HTML files, copying legitimate files to a new folder
+#                 Necessary because Egypt Independent's Drupal installation served up multiple 
+#                 versions of each page (probably because of Views), and httrack added a bunch 
+#                 of temporary files
+# Author:         Andrew Heiss
+# Last updated:   2013-07-01
+# Python version: ≥3.0
+# Usage:          Edit the two variables below and run the script
+#                 Manual hand-holding: I'm too lazy to programmatically filter out directories, 
+#                   so the script chokes every time it comes across one. When that happens, just 
+#                   delete the folder (and any corresponding folder.tmp or folder.html) files and
+#                   run the script again. (Or add a check for directories or something :) )
+
+#---------
+# Set up
+#---------
+files_to_clean = 'egind_test/*'  # Must have trailing /*
+folder_for_clean_files = 'egind_clean'  # Just the name of the folder (no trailing /)
+
+
 # Import modules
 from os import path, makedirs
 from shutil import copy2
 from glob import glob
-
-# Set up
-files_to_clean = 'egind_test/*'  # Must have trailing /*
-folder_for_clean_files = 'egind_clean'  # Just the name of the folder
 
 # Strings to ignore (weird things added by Drupal and/or httrack in Egypt Independent)
 ignore_these = ('.tmp', '2d85.html', 'b6c9.html', 'ed36.html')
