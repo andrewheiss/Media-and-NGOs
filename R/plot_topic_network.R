f3597a70a15fbc16b341f35e3f6d044d53e0b9d6
Andrew Heiss <andrewheiss@gmail.com>
2013-12-28 15:02:32 -0500

    Add dendrogram + bar charts and finish fancy plot

--- R/plot_topic_network.R
+++ R/plot_topic_network.R
@@ -1,53 +1,119 @@
+# Title:          plot_topic_network.R
+# Description:    Plot an arcplot and dendrogram of the topic model
+# Author:         Andrew Heiss
+# Last updated:   2013-12-28
+# R version:      ≥3.0
 
-# http://rpubs.com/gaston/dendrograms
-# http://wheatoncollege.edu/lexomics/files/2012/08/How-to-Read-a-Dendrogram-Web-Ready.pdf
+# Huge thanks to Rolf Fredheim for creating the arcplot + dendrogram + topic proportions idea!
+# http://quantifyingmemory.blogspot.com/2013/11/visualising-structure-in-topic-models.html
 
-# Load topic model
-load("topic_model.RData")
+# Useful dendrogram resources
+#   * http://rpubs.com/gaston/dendrograms
+#   * http://wheatoncollege.edu/lexomics/files/2012/08/How-to-Read-a-Dendrogram-Web-Ready.pdf
 
-# Add publication name
-topic.docs.norm$publication <- factor(regmatches(row.names(topic.docs.norm), regexpr("^[^_]+", row.names(topic.docs.norm))), 
-                                      labels=c("Al-Ahram English", "Daily News Egypt", "Egypt Independent"))
 
-# Create edge list for arcplot
+# Load libraries
 library(ggplot2)
 library(plyr)
 library(reshape2)
 library(ggdendro)
 library(arcdiagram)
 
+# Load topic model
+load("topic_model.RData")
+
+
+#-----------------------------------------
+# Create correlation and dendrogram data
+#-----------------------------------------
+# Create data frames
+df <- topic.docs.norm
+df.publication <- df
+df.publication$publication <- factor(regmatches(row.names(df), regexpr("^[^_]+", row.names(df))), 
+                                     labels=c("Al-Ahram English", "Daily News Egypt", "Egypt Independent"))
+
 # Create correlation matrix
-df <- topic.docs.norm[,-which(names(topic.docs.norm) == "publication")]  # No publication data 
 cors <- cor(df)
-rownames(cors) <- topic.keys.result$V3
-colnames(cors) <- topic.keys.result$V3
+rownames(cors) <- topic.keys.result$short.names
+colnames(cors) <- topic.keys.result$short.names
 
 # Create cluster object from matrix
-hc <- hclust(dist(cors), "ward")
-plot(hc)
-hc$order
-# Convert cluster to dendrogram
-hc2 <- as.dendrogram(hc)
-order.dendrogram(hc2)
-ggdendrogram(hc2, rotate=TRUE, theme_dendro=FALSE)
+cor.cluster <- hclust(dist(cors), "ward")
+cor.dendro <- as.dendrogram(cor.cluster)  # Convert cluster to dendrogram
 
+
+#-----------------
+# Create arcplot
+#-----------------
 # Get pieces for arcplot
 values <- colSums(cors)  # Sum of correlations for edge values
-out <- melt(cors)  # Convert to long
-out <- out[out[,3] >= 0, ]  # Get rid of negative correlations
-edgelist <- as.matrix(out[,1:2])  # Create edgelist
+edges <- melt(cors)  # Convert to long
+edges <- out[out[, 3] >= 0, ]  # Get rid of negative correlations
+colnames(edges) <- c("Source","Target","Weight")
+edgelist <- as.matrix(edges[, 1:2])  # Create edgelist
 
-# Plot with the width of the arcs proportional to the values
-arcplot(edgelist, lwd.arcs=20 * out[,3], show.nodes=TRUE, show.labels=FALSE, ordering=hc$order)
+# Replicate the dendrogram order
+arcs.order <- order.dendrogram(cor.dendro)
 
-# Get pie graph bands
-bands <- ddply(topic.docs.norm, ~ publication, function(x) sqrt(colSums(x[,-which(names(x) == "publication")])))
-rownames(bands) <- bands[,1]  # Set publication names as row names
-bands[,1] <- NULL  # Remove publication column
-bands <- t(bands)  # Convert to long
-bands
+# Plot the arcplot
+pdf(file="../Output/arcs.pdf", width=3, height=5)
+arcplot(edgelist, lwd.arcs=20 * edges[,3], 
+        show.nodes=TRUE, sorted=TRUE, ordering=arcs.order, 
+        col.labels="red", col.arcs="#ff7f00", horizontal=FALSE)
+dev.off()
+# Bonus!
+# This will rotate the graph by 180°, but it doesn't work in RStudio, and it can't flip the graph.
+# So we need to use Photoshop to get the arcs and dendrogram to align
+# library(grid)
+# cap <- grid.cap()
+# grid.newpage()
+# grid.raster(cap, vp=viewport(angle=180))
 
-col.bands <- c("#E41A1C", "#377EB8", "#12345D")
-arcBandBars(edgelist, bands,
-            col.bands=col.bands, lwd=out[,3]*40, col=cols,
-            col.terms="white", mar=c(1,1,3,1))
+
+#----------------------------------------
+# Plot dendrogram and topic proportions
+#----------------------------------------
+# Scaling and normalizing functions
+# Scale the data between exactly 0 and 1
+scale.data <- function(X) {
+  (X - min(X)) / diff(range(X))
+}
+
+# Square root of the column sums
+sqrt.sum <- function(x) {
+  sqrt(sum(x))
+}
+
+# Reorder topics to match the dendrogram
+topic.order <- order.dendrogram(cor.dendro)  # Get order of dendrogram
+column.names <- colnames(df)  # Get column names from publication-free data frame
+topic.order <- column.names[topic.order]  # Get correct column order
+
+# Create data frame for manually plotting the dendrogram
+dendro <- ggdendro:::dendrogram_data(cor.dendro)  # Extract ggplot data frame from dendrogram object
+dendro$segments$yend[dendro$segments$yend < 0.8] <- 0.8  # Cut ends of leaves off to make room for bar charts
+
+# Create data frame for plotting different proprotions
+# Proportion means
+topic.means.wide <- ddply(df.publication, ~ publication, colwise(mean))
+topic.means.long <- melt(topic.means.wide, id="publication", variable.name="topic", value.name="mean.prop")
+
+# Square root of column sums
+topic.sqrt.wide <- ddply(df.publication, ~ publication, colwise(sqrt.sum))
+topic.sqrt.long <- melt(topic.sqrt.wide, id="publication", variable.name="topic", value.name="sqrt.sum")
+
+# Combine into one data frame
+plot.data <- topic.means.long
+plot.data$topic <- factor(plot.data$topic, levels=topic.order, ordered=TRUE)
+plot.data$scaled0to1 <- scale.data(plot.data$mean.prop) / 2  # Scale data
+plot.data$sqrt.sum <- topic.sqrt.long$sqrt.sum / 2.4
+
+# Plot the dendrogram and bar plots
+p <- ggplot() + geom_segment(data=segment(dendro), aes(x=x, y=y, xend=xend, yend=yend)) +
+  theme_dendro() + coord_flip() +
+  theme(axis.text.y=element_text(hjust=1, size=13)) +
+  scale_x_discrete(labels=dendro$labels$label) +
+  scale_fill_brewer(palette="Set1", name="") + 
+#   geom_bar(data=plot.data, aes(topic, sqrt.sum, fill=publication), stat="identity", width=.5)
+  geom_bar(data=plot.data, aes(topic, scaled0to1, fill=publication), stat="identity", width=.5)
+ggsave(plot=p, filename="../Output/dendro.pdf", width=7, height=5, units="in")
